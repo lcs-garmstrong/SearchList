ec6444b0e061c8763a574e55c6bf4e9ab1447eff
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-04-11 08:51:35 -0400

    Initial Commit

---
+++ SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by Graeme Armstrong on 2023-04-11.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
